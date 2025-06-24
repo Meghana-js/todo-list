aad1952477aa4e10d421f4b696642c35c4008ce4
Meghana-js <meghanajs731@gmail.com>
2025-06-24 14:18:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 script.js
-const todoList = document.getElementById('todo-list');
-const todoInput = document.getElementById('todo-input');
-const addTodoBtn = document.getElementById('add-todo-btn');
+const todoItems = document.querySelectorAll('.todo-item');
 
-addTodoBtn.addEventListener('click', () => {
-	const newTodo = document.createElement('li');
-	newTodo.textContent = todoInput.value;
-	todoList.appendChild(newTodo);
-	todoInput.value = '';
+todoItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('flipped');
+    });
 });
